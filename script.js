d108d68144c62af0f1d67b003fc9b93ddbbf0db9
Jeana Rose <jr.mathis608@gmail.com>
2020-12-09 17:36:44 -0500

    Modified generatePassword function, added if statements, and object

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Global variables so that all functions can reach them while generating and writing password
+var lowerCase = ["a", "b", "c", "d"];
+var upperCase = ["A", "B", "C", "D"];
+var numbers = ["1", "2", "3"];
+var special = ["!", "@", "#"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,22 +16,58 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var password = [];
-  var lowercase = prompt("Y / N: Do you want to include lowercase characters?");
+  var passwordLength = prompt(
+    "How many characters do you want your password to be?"
+  );
+  var lowerCaseOption = confirm("Do you want to include lowercase characters?");
+  var upperCaseOption = confirm("Do you want to include uppercase characters?");
+  var numbersOption = confirm("Do you want to include numbers characters?");
+  var specialCharactersOption = confirm(
+    "Do you want to include special characters?"
+  );
 
-  if (lowercase === "y") {
-    // Generate random lowercase
-    console.log("Your password included lowercase characters!");
-    var alphabetLowercase = "abcdefghijklmnopqrstuvwxyz";
-    var randomLowercase =
-      alphabetLowercase[Math.floor(Math.random() * alphabetLowercase.length)];
-    password.push(randomLowercase)
-    console.log(randomLowercase)
-  } else {
-    // Do not include lowercase characters in password
-    console.log("Your password did not include lowercase characters!");
-    return "";
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Oops, your password needs to be between 8 and 128 characters!");
+    return;
   }
+
+  if (passwordLength !== typeof number) {
+    alert("Oops, that wasn't a number!");
+    return;
+  }
+
+  if (
+    lowerCaseOption === false &&
+    upperCaseOption === false &&
+    numbersOption === false &&
+    specialCharactersOption === false
+  ) {
+    alert("Oops, you didn't select any password criteria!");
+    return;
+  }
+
+  var chosenCriteria = {
+    passwordLength: passwordLength,
+    lowerCaseOption: lowerCaseOption,
+    upperCaseOption: upperCaseOption,
+    numbersOption: numbersOption,
+    specialCharactersOption: specialCharactersOption
+  }
+  return chosenCriteria;
+
+  // if (lowerCaseOption) {
+  //   // Generate random lowercase
+  //   console.log("Your password included lowercase characters!");
+  //   var randomLowercase =
+  //     lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  //   password.push(randomLowercase);
+  //   console.log(password);
+  //   return password;
+  // } else {
+  //   // Do not include lowercase characters in password
+  //   console.log("Your password did not include lowercase characters!");
+  //   return "";
+  // }
 }
 
 // Add event listener to generate button
