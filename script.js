43aac82de6ac565080c670e600a8b9e9ce32e0e6
Jeana Rose <jr.mathis608@gmail.com>
2020-12-08 19:43:10 -0500

    Added variables for password criteria to the function passwordCritera.

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ function generatePassword(){
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+function passwordCriteria() {
+  var characters = prompt("How many characters do you want your password to be?");
+  var lowercase = prompt("Do you want to include lowercase characters?");
+  var uppercase = prompt("Do you want to include uppercase characters?");
+  var numeric = prompt("Do you want to include numeric characters?");
+  var special = prompt ("Do you want to include special characters?");
+
+ 
+}
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
